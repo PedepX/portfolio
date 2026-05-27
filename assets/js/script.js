6d771b855f03a2d57ec58b1320939af24b85cdd8
Joao Pedro de Menezes Almeida <joao59799876@aluno.ba.senac.br>
2026-05-27 16:52:14 -0300

    feat:updated

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+//console.log("Hello, World!");
+//alert("Hello, World!");
+//if (true) {
+//let nome = "Pedro";
+//console.log(nome);
+//}
+//const nome = "Pedro";
+//let idade = 21;
+//idade = 22;
+
+//console.log(nome);
+//console.log(idade);
+
+//const mediafile = "storage/media/file";
+
+//let estado;
+//if (true) {
+//    estado = "SP";
+//}
+//console.log(estado);
+
+//let nome = "Pedro"; console.log(nome); console.log(typeof nome);
+
+//let nome = prompt("Digite seu nome:"); console.log(nome);
+
+//let resposta = confirm("Deseja sair?"); console.log(resposta);
+
+//let soma = 10 + 5; console.log(soma);
+
+//let numero1 = 10;
+//let numero2 = 5;
+
+//console.log(numero1 + numero2); // 15
+//console.log(numero1 - numero2); // 5
+//console.log(numero1 * numero2); // 50
+//console.log(numero1 / numero2); // 2
+//console.log(numero1 % numero2); // 0
+
+//let nome = "Pedro"; console.log("Olá, " + nome);
+//let nome = "Pedro";console.log(`Olá, ${nome}`);
