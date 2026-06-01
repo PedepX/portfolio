ccbdd94259cb758ff10535dfadefca493e0a6b51
Joao Pedro de Menezes Almeida <joao59799876@aluno.ba.senac.br>
2026-06-01 16:48:59 -0300

    agora é só isso mesmo

--- assets/js/DOM.js
+++ assets/js/DOM.js
@@ -1,6 +1,15 @@
 // Seleciona todos os itens
-const itens =
-document.querySelectorAll('.item');
+// const itens =
+// document.querySelectorAll('.item');
 
 //Resultado: NodeList(3)
-console.log(itens);
+//console.log(itens);
+
+itens.forEach(function(item) {
+
+    itens[1].style.fontWeight = 'bold';
+
+    item.addEventListener('click', function() {
+        console.log("Clicou no item: " + item.innerText);
+    });
+})
